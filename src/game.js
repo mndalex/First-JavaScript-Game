6c05279199c95d78783d72ad0a2a1669f0013591
MnD4lex <alex@ticketingexp.com>
2020-03-31 12:46:25 +0200

    Következő level betöltése implementálva

--- src/game.js
+++ src/game.js
@@ -2,12 +2,7 @@ import Paddle from "./paddle.js";
 import Ball from "./ball.js";
 import InputHandler from "./input.js";
 import { buildLevel, levels } from "./levels.js";
-
-const GAME_STATE = {
-  RUNNING: 1,
-  PAUSED: 2,
-  GAME_OVER: 3
-};
+import { GAME_STATE } from "./game_states.js";
 
 export default class Game {
   constructor(gameWidth, gameHeight, brickSize) {
@@ -16,13 +11,14 @@ export default class Game {
     this.brickSize = brickSize;
 
     this.gameState = GAME_STATE.RUNNING;
+    this.level = 0;
   }
 
   start() {
     this.paddle = new Paddle(this);
     this.ball = new Ball(this);
 
-    this.bricks = buildLevel(this, levels[0], this.brickSize);
+    this.loadlevel(this.level);
 
     this.gameObjects = [this.paddle, this.ball];
 
@@ -30,12 +26,27 @@ export default class Game {
   }
 
   update(dt) {
-    if (this.gameState == GAME_STATE.PAUSED) return;
+    if (this.gameState === GAME_STATE.RUNNING) {
+      if (Object.keys(this.bricks).length === 0)
+        if (levels.length - 1 > this.level)
+          this.gameState = GAME_STATE.LEVEL_COMPLETED;
+        else this.gameState = GAME_STATE.ALL_LEVEL_COMPLETED;
 
-    this.gameObjects.forEach(object => object.update(dt));
+      this.gameObjects.forEach(object => object.update(dt));
+    }
   }
 
   draw(ctx) {
+    if (this.gameState === GAME_STATE.LEVEL_COMPLETED) {
+      this.drawLevelCompleted(ctx);
+      return;
+    }
+
+    if (this.gameState === GAME_STATE.ALL_LEVEL_COMPLETED) {
+      this.drawAllLevelCompleted(ctx);
+      return;
+    }
+
     for (let key in this.bricks) {
       this.bricks[key].draw(ctx);
     }
@@ -48,23 +59,27 @@ export default class Game {
   }
 
   togglePause() {
-    if (this.gameState == GAME_STATE.PAUSED) {
+    if (this.gameState === GAME_STATE.PAUSED) {
       this.gameState = GAME_STATE.RUNNING;
     } else {
       this.gameState = GAME_STATE.PAUSED;
     }
   }
 
+  loadNextLevel() {
+    this.loadlevel(++this.level);
+    this.resetPaddle();
+    this.resetBall();
+    this.gameState = GAME_STATE.RUNNING;
+  }
+
   getBrickByPosition(x, y) {
     let brickPosition = {
-      x:
-        Math.floor(x / this.brickSize) * this.brickSize + 1,
+      x: Math.floor(x / this.brickSize) * this.brickSize + 1,
       y: Math.floor(y / this.brickSize) * this.brickSize + 1
     };
 
-    return this.bricks[
-      brickPosition.x + "-" + brickPosition.y
-    ];
+    return this.bricks[brickPosition.x + "-" + brickPosition.y];
   }
 
   deleteBrickByPosition(position) {
@@ -83,5 +98,62 @@ export default class Game {
       this.gameWidth / 2,
       this.gameHeight / 2
     );
+  };
+
+  loadlevel = function(level) {
+    if (levels[level] != null) {
+      this.bricks = buildLevel(
+        this,
+        levels[level],
+        this.brickSize
+      );
+    }
+  };
+
+  drawLevelCompleted = function(ctx) {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+    ctx.fillRect(0, 0, this.gameWidth, this.gameHeight);
+
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText(
+      "Level Completed!",
+      this.gameWidth / 2,
+      this.gameHeight / 2
+    );
+    ctx.fillText(
+      "Press Space to continue.",
+      this.gameWidth / 2,
+      this.gameHeight / 2 + 35
+    );
+  };
+
+  drawAllLevelCompleted = function(ctx) {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+    ctx.fillRect(0, 0, this.gameWidth, this.gameHeight);
+
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText(
+      "All Level Completed!",
+      this.gameWidth / 2,
+      this.gameHeight / 2
+    );
+  };
+
+  resetPaddle = function() {
+    this.paddle.position = {
+      x: this.gameWidth / 2 - this.paddle.size.x / 2,
+      y: this.gameHeight - this.paddle.size.y - 10
+    };
+  };
+
+  resetBall = function() {
+    this.ball.position = {
+      x: this.paddle.position.x + this.paddle.size.x / 2,
+      y: this.paddle.position.y
+    };
   };
 }
