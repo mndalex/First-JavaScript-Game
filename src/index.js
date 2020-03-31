a0d1808665fad8182da9b9838a164c7fb145dcd7
MnD4lex <alex@ticketingexp.com>
2020-03-31 10:59:52 +0200

    FPS mérő implementálva

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import Game from "./game.js";
 
 let canvas = document.getElementById("game-screen");
+let fpsCounter = document.getElementById("fps-counter");
 let ctx = canvas.getContext("2d");
 
 const GAME_WIDTH = canvas.width;
 const GAME_HEIGHT = canvas.height;
-const BRICKS_SIZE = 50; 
+const BRICKS_SIZE = 50;
 
 let game = new Game(GAME_WIDTH, GAME_HEIGHT, BRICKS_SIZE);
 
 game.start();
 
 let lastTime = 0;
+let lastFPSCalc = 0;
 
 function gameLoop(timestamp) {
   let dt = (timestamp - lastTime) / 1000;
   lastTime = timestamp;
+
+  if (timestamp - lastFPSCalc > 150) {
+    let fps = Math.round(1 / dt);
+    fpsCounter.innerHTML = fps;
+    lastFPSCalc = timestamp;
+  }
 
   ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
